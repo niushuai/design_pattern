a8e4f17f7d2361d57bdaaa64033f8fc68859715e
niushuai <niushuai666@gmail.com>
2015-11-28 21:39:15 +0800

    增加一个改进点

--- src/_10_Iterator/Waitness.java
+++ src/_10_Iterator/Waitness.java
@@ -1,6 +1,5 @@
 package _10_Iterator;
 
-
 /**
  * 服务员类。虽然餐厅菜单项目不一样。但是应用迭代器设计模式以后就被解耦了
  * 
@@ -17,6 +16,9 @@ public class Waitness {
         this.diner = diner;
     }
 
+    // 思考一下，这里又什么坏处？答案是违反了“对修改关闭，对扩展开放”的原则。如果还有一个新的<br>
+    // 菜单，那么打印菜单这里就还是需要修改。最简单的方式是使用一个 ArrayList 支持拓展，但更好的方式<br>
+    // 是下面将要学习的——组合模式
     public void printMenu() {
         Iterator pancakeIterator = pancake.getMenuIterator();
         Iterator dinerIterator = diner.getMenuIterator();
